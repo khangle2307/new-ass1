49cd204b4728ef6b4917b09d02bb5a005fe688e7
khangle <khanglmph14697@gmail.com>
2022-02-11 15:43:45 +0700

    update searchbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ const Navbar = {
            <li><a href="#" class="text-white px-2">Phụ kiện</a></li>
         </ul>   
         <div class="flex my-1">
-            ${Searchbar.render}
+            ${Searchbar.render()}
            <button class="btn-blue">Đăng nhập</button>
         </div>   
      </div>
