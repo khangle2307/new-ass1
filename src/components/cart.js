8102512185369a348643c387f749768114fc3f78
khangle <khanglmph14697@gmail.com>
2022-02-14 15:16:57 +0700

    feat:cart.js

---
+++ src/components/cart.js
@@ -0,0 +1,14 @@
+import "remixicon/fonts/remixicon.css";
+
+const Cart = {
+    render() {
+        return /* html */ `
+            <div class="mx-3">
+                <i class="ri-shopping-bag-line text-white relative text-3xl"></i>
+                <p class="text-white absolute top-0">0</p>
+            </div>
+     `;
+    },
+};
+
+export default Cart;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,19 @@
-import Searchbar from "./searchbar";
+import Cart from "./cart";
 
 const Navbar = {
     render() {
         return /* html */`
-     <div class="flex justify-center items-center py-3">
+     <div class="flex justify-around items-center py-3">
         <a href="/" class="text-lg text-white uppercase font-semibold px-5">Shopbye</a>         
         <ul class="flex">
-           <li><a href="#" class="text-white px-2">Trang chủ</a></li>
-           <li><a href="#" class="text-white px-2">Điện thoại</a></li>
-           <li><a href="#" class="text-white px-2">Laptop,pc,máy tính</a></li>
-           <li><a href="#" class="text-white px-2">Phụ kiện</a></li>
+           <li><a href="/phone" class="text-white px-2">Điện thoại</a></li>
+           <li><a href="/laptop" class="text-white px-2">Laptop,pc,máy tính</a></li>
+           <li><a href="/phukien" class="text-white px-2">Phụ kiện</a></li>
+           <li><a href="/dongho" class="text-white px-2">Đồng hồ</a></li>
         </ul>   
-        <div class="flex my-1">
-            ${Searchbar.render()}
+        <div class="flex my-1 mx-4">
            <button class="btn-blue">Đăng nhập</button>
+           ${Cart.render()}
         </div>   
      </div>
     `;
